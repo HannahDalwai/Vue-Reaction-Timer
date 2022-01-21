2b5ae96cb57e7ee01e35332f1816f8df6cc02dd4
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-21 14:03:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<h1>Reaction Timer</h1>
+<button @click="start" :disabled= "isPlaying">Play</button>
+<Block v-if = "isPlaying" :delay='delay'/>
+</template>
+
+<script>
+
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  components: {Block},
+    data(){
+      return{
+        isPlaying:false,
+        delay:null    
+      }
+    },
+
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,33 @@
+<template>
+<div class = "block" v-if="showBlock"> Click me </div>
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showBlock:false
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+            this.showBlock = true
+        }, this.delay)
+    },
+  
+}
+</script>
+<style >
+
+   .block{
+       width: 400px;
+       border-radius:20px;
+       background: #AD846B;
+       color:white;
+       text-align:center;
+       padding:100px 0;
+       margin:40px auto;
+
+   } 
+</style>
